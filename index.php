a613d69d511f6e9f60866641c6f2963e11c2f875
jackieshraw <jackieshraw@users.noreply.github.com>
2020-07-29 10:04:40 +0300

    sd

---
+++ index.php
@@ -0,0 +1,248 @@
+﻿<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+	<meta name="description" content="Namalsk RolePlay - Играй в GTA Criminal Russia по сети"/>
+	<meta name="keywords" content="crmp, samp, gta, sa, gta sa, multiplayer, namalsk, roleplay, rp, server, project, крмп, самп, гта, са, гта са, мультиплеер, намальск, ролеплей, ролеплэй, рп, сервер, проект"/>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="alternate" hreflang="ru" href="default.htm" />
+	<meta name="author" content="Pavel Serdyuk">
+	<meta name="yandex-verification" content="b14b8b214c673f18" />
+	<script async src="../https@www.googletagmanager.com/gtag/js@id=UA-115893046-2"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-115893046-2');
+	</script>
+	<link rel="stylesheet" href="addons/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' href='css/animate.css'>
+	<link rel='stylesheet' href='css/reset.css'>
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+<div class='mobile-closeicon'></div>
+<div class='mobile-wrapper'>
+	<div class='mobile-closefield'></div>
+	<div class='mobile-block'>
+		<a href='index.php'><div class='logo'></div></a>
+		<ul class='menu'>
+			<a href='index.php'><li class='link' name=''>Главная</li></a>
+			<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li class='link' >Форум</li></a>
+			<a href='start.php'><li class='link' name='start.php'>Как играть?</li></a>
+			<a href='about.php'><li class='link' name='about.php'>О проекте</li></a>
+			<a href='donate.php'><li class='link' name='donate.php'>Донат</li></a>
+			 
+			<a name='login' href='login.php'>
+				<div class='login-box'>
+					<div class='avatar'><div class='img'></div></div>
+					<p>Войти</p>
+				</div>
+			</a>
+								</ul>
+		<h2 class='copyright'>
+			NAMALSK TEAM 2019
+		</h2>
+	</div>
+</div>
+<div class='header'>
+	<div class='header-content'>
+		<a href='index.php'><div class='logo'></div></a>
+		<ul class='menu'>
+			<a href='index.php'><li class='link' name=''>Главная</li></a>
+			<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li class='link' >Форум</li></a>
+			<a href='start.php'><li class='link' name='start'>Как играть?</li></a>
+			<a href='about.php'><li class='link' name='about'>О проекте</li></a>
+			<a name='donate' href='donate.php'><li class='donate'>ДОНАТ</li></a>
+			<a name='start' href='start.php'><li class='start'>НАЧАТЬ ИГРАТЬ</li></a>
+			 
+			<a name='login.php' href='login.php'>
+				<div class='login-box'>
+					<div class='avatar'><div class='img'></div></div>
+					<p>Войти</p>
+				</div>
+			</a>
+								</ul>
+	</div>
+</div><html>
+	<meta property="og:title" content="Namalsk RolePlay - Играй в GTA Criminal Russia по сети" /> 
+	<meta property="og:image" content="https://namalsk-rp.ru/img/vk/main.png" />
+	<title>Namalsk RolePlay - Играй в GTA Criminal Russia по сети</title>
+	<link rel='stylesheet' href='css/main.css'>
+		<body>
+		<div class='block welcome'>
+			<div class='desc'>
+				<h2 class='title'>
+					Криминальная<br>
+					<span name='one'>Россия</span> <span name='two'>Ждёт тебя!</span>
+				</h2>
+				<h3>НЕ ЗНАЕШЬ КАК НАЧАТЬ ИГРАТЬ?</h3>
+				<a href='start.php'><button>Перейти к пособию</button></a>
+				<div class='particle one'></div>
+				<div class='particle two'></div>
+				<div class='particle three'></div>
+			</div>
+			<div class='laptop'>
+				<div class='back-1'></div>
+				<div class='back-2'></div>
+				<div class='laptop-block'>
+					<div class='img'></div>
+					<div class='play'>
+						<div class='circle'>
+							<div class='icon'></div>
+						</div>
+					</div>
+					<div class='tooltip'>
+						<h2>Узнай больше!</h2>
+						<h3>Посмотреть промо-ролик</h3>
+					</div>
+				</div>
+				<div class='particle one'></div>
+				<div class='particle two'></div>
+			</div>
+			<div class='video'>
+				
+			</div>
+			<div class='particle'></div>
+		</div>
+		<div class='block servers'>
+			<h2>НАШИ СЕРВЕРА</h2>
+			<h3>ОБЩИЙ ОНЛАЙН: <span>0</span></h3>
+			<div class='servers-content'>
+				<div class='server one'>
+					<h3 class='status'>СЕРВЕР ONLINE</h3>
+					<p><span>01</span><br>СЕР<br>ВЕР</p>
+					<div class='img'></div>
+					<div class='desc'>
+						<div class='online'>
+							<div class='icon'></div>
+							<p>0/443</p>
+						</div>
+						<h2>NAMALSK<br>ROLEPLAY 1</h2>
+						<h3>194.61.44.41:8904</h3>
+					</div>
+				</div>
+				<div class='server two'>
+					<h3 class='status'>СЕРВЕР ONLINE</h3>
+					<p><span>02</span><br>СЕР<br>ВЕР</p>
+					<div class='img'></div>
+					<div class='desc'>
+						<div class='online'>
+							<div class='icon'></div>
+							<p>0/443</p>
+						</div>
+						<h2>NAMALSK<br>ROLEPLAY 2</h2>
+						<h3>194.61.44.42:8904</h3>
+					</div>
+				</div>
+				<div class='server three'>
+					<h3 class='status'>СЕРВЕР ONLINE</h3>
+					<p><span>03</span><br>СЕР<br>ВЕР</p>
+					<div class='img'></div>
+					<div class='desc'>
+						<div class='online'>
+							<div class='icon'></div>
+							<p>0/443</p>
+						</div>
+						<h2>NAMALSK<br>ROLEPLAY 3</h2>
+						<h3>194.61.44.43:8904</h3>
+					</div>
+				</div>
+				<div class='server four'>
+					<h3 class='status'>СЕРВЕР ONLINE</h3>
+					<p><span>04</span><br>СЕР<br>ВЕР</p>
+					<div class='img'></div>
+					<div class='desc'>
+						<div class='online'>
+							<div class='icon'></div>
+							<p>0/443</p>
+						</div>
+						<h2>NAMALSK<br>ROLEPLAY 4</h2>
+						<h3>194.61.44.44:8904</h3>
+					</div>
+				</div>
+			</div>
+			<div class='radio'>
+				<p>Radio<br><span>Namalsk</span></p>
+				<div class='play'>
+					<div class='microphone'></div>
+					<div class='activator'></div>
+					<div class='loader'><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i></div>
+				</div>
+				<div class='waves'>
+					<div class='wave one'></div>
+					<div class='wave two'></div>
+					<div class='wave three'></div>
+					<div class='wave four'></div>
+					<div class='wave five'></div>
+					<div class='wave six'></div>
+					<div class='wave seven'></div>
+					<div class='wave eight'></div>
+				</div>
+			</div>
+		</div>
+		<div class='block social'>
+			<div class='line'></div>
+			<h2>Следи за проектом<br>в социальных сетях</h2>
+			<div class='socials-wrapper'>
+				<div class='social-block'>
+					<div class='avatar' style='background-image:url(../https@sun6-1.userapi.com/c851224/v851224652/7171b/l3TDQg21DRI.jpg@ava=1);'></div>
+					<h2>Официальное<br>сообщество</h2>
+					<h3>Все главные новости касательно проекта</h3>
+					<div class='subs'>
+						<div class='icon'></div>
+						<p>0</p>
+					</div>
+					<a href='../https@vk.com/namalskrp' target='_blank'><button>Вступить</button></a>
+				</div>
+				<div class='social-block'>
+					<div class='avatar' style='background-image:url(../https@sun6-1.userapi.com/c849136/v849136732/feddd/K2K2m33CqI4.jpg@ava=1);'></div>
+					<h2>Сообщество<br>игроков</h2>
+					<h3>Группа для общения с игроками</h3>
+					<div class='subs'>
+						<div class='icon'></div>
+						<p>0</p>
+					</div>
+					<a href='../https@vk.com/namalsk_free' target='_blank'><button>Вступить</button></a>
+				</div>
+				<div class='social-block'>
+					<div class='avatar' style='background-image:url(../https@sun6-4.userapi.com/c834400/v834400475/f82a1/409Zm_G9LyI.jpg@ava=1);'></div>
+					<h2>Радиостанция<br>проекта</h2>
+					<h3>Играй под ритм нашего радио</h3>
+					<div class='subs'>
+						<div class='icon'></div>
+						<p>0</p>
+					</div>
+					<a href='../https@vk.com/namalskradio' target='_blank'><button>Вступить</button></a>
+				</div>
+			</div>
+		</div>
+		<div class='footer'>
+	<div class='content'>
+		<a href='default.htm'><div class='logo'></div></a>
+		<div class='menus'>
+			<ul class='menu'>
+				<a href='start.php'><li>НАЧАТЬ ИГРАТЬ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li>ФОРУМ</li></a>
+				<a href='donate.php'><li>ДОНАТ</li></a>
+				<a target='_blank' href='../https@vk.com/24templates.ru'><li>МЫ ВКОНТАКТЕ</li></a>
+			</ul>
+			<ul class='menu'>
+				<a href='about.php'><li>О ПРОЕКТЕ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/threads/_25D0_259F_25D1_2580_25D0_25B0_25D0_25B2_25D0_25B8_25D0_25BB_25D0_25B0-_25D0_25BF_25D1_2580_25D0_25BE_25D0_25B5_25D0_25BA_25D1_2582_25D0_25B0-namalsk-roleplay.21714/default.htm'><li>ПРАВИЛА ПРОЕКТА</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/misc/contact'><li>ОБРАТНАЯ СВЯЗЬ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/forums/_25D0_2591_25D0_25B0_25D0_25B7_25D0_25B0-_25D0_25B7_25D0_25BD_25D0_25B0_25D0_25BD_25D0_25B8_25D0_25B9-f-a-q.6/default.htm'><li>ПОМОЩЬ</li></a>
+			</ul>
+		</div>
+	</div>
+	<h2 class='copyright'>
+		NAMALSK TEAM 2019
+	</h2>
+</div>
+<script src='js/jquery-3.2.1.min.js' type='text/javascript'></script>
+<script src='js/classie.js' type='text/javascript'></script>
+<script src='js/common.js' type='text/javascript'></script>
+<script src='js/main.js' type='text/javascript'></script>
+<!-- made by Pavel Serdyuk -->		<script src='js/index.js' type='text/javascript'></script>
+	</body>
+</html>
---
+++ start.php
@@ -0,0 +1,144 @@
+﻿<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+	<meta name="description" content="Namalsk RolePlay - Играй в GTA Criminal Russia по сети"/>
+	<meta name="keywords" content="crmp, samp, gta, sa, gta sa, multiplayer, namalsk, roleplay, rp, server, project, крмп, самп, гта, са, гта са, мультиплеер, намальск, ролеплей, ролеплэй, рп, сервер, проект"/>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="alternate" hreflang="ru" href="default.htm" />
+	<meta name="author" content="Pavel Serdyuk">
+	<meta name="yandex-verification" content="b14b8b214c673f18" />
+	<script async src="../https@www.googletagmanager.com/gtag/js@id=UA-115893046-2"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-115893046-2');
+	</script>
+	<link rel="stylesheet" href="addons/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' href='css/animate.css'>
+	<link rel='stylesheet' href='css/reset.css'>
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+<div class='mobile-closeicon'></div>
+<div class='mobile-wrapper'>
+	<div class='mobile-closefield'></div>
+	<div class='mobile-block'>
+		<a href='default.htm'><div class='logo'></div></a>
+		<ul class='menu'>
+			<a href='index.php'><li class='link' name=''>Главная</li></a>
+			<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li class='link' >Форум</li></a>
+			<a href='start.php'><li class='link' name='start'>Как играть?</li></a>
+			<a href='about.php'><li class='link' name='about'>О проекте</li></a>
+			<a href='donate.php'><li class='link' name='donate'>Донат</li></a>
+			 
+			<a name='login.php' href='login'>
+				<div class='login-box'>
+					<div class='avatar'><div class='img'></div></div>
+					<p>Войти</p>
+				</div>
+			</a>
+								</ul>
+		<h2 class='copyright'>
+			NAMALSK TEAM 2019
+		</h2>
+	</div>
+</div>
+<div class='header'>
+	<div class='header-content'>
+		<a href='default.htm'><div class='logo'></div></a>
+		<ul class='menu'>
+			<a href='index.php'><li class='link' name=''>Главная</li></a>
+			<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li class='link' >Форум</li></a>
+			<a href='start.php'><li class='link' name='start'>Как играть?</li></a>
+			<a href='about.php'><li class='link' name='about'>О проекте</li></a>
+			<a name='donate.php' href='donate'><li class='donate'>ДОНАТ</li></a>
+			<a name='start.php' href='start'><li class='start'>НАЧАТЬ ИГРАТЬ</li></a>
+			 
+			<a name='login.php' href='login'>
+				<div class='login-box'>
+					<div class='avatar'><div class='img'></div></div>
+					<p>Войти</p>
+				</div>
+			</a>
+								</ul>
+	</div>
+</div><html>
+	<meta property="og:title" content="Начать играть - Namalsk RolePlay - Играй в GTA Criminal Russia по сети" /> 
+	<meta property="og:image" content="https://namalsk-rp.ru/img/vk/start.png" />
+	<title>Начать играть - Namalsk RolePlay - Играй в GTA Criminal Russia по сети</title>
+	<link rel='stylesheet' href='css/start.css'>
+	<body>
+		<div class='block startplay'>
+			<div class='content'>
+				<div class='main-content'>
+					<h2>НАЧАТЬ ИГРАТЬ<br>ПРОСТО!</h2>
+					<h3>
+						Мы разработали собственный лаунчер, который сам
+						установит нужные компоненты игры. Вам нужно будет
+						только ввести игровой никнейм в формате
+						Имя_Фамилия и подключиться к серверу.
+					</h3>
+					<a href='download/nrp-launcher.exe' target='_blank'><button>ЗАГРУЗИТЬ ЛАУНЧЕР</button></a>
+					<p>СИСТЕМНЫЕ ТРЕБОВАНИЯ</p>
+					<div class='reqs'>
+						<div class='req'>
+							<h2>Минимальные:</h2>
+							<h3>
+								ОС: Microsoft Windows 7, 8, 10<br>
+								Процессор: Pentium III или AMD Athlon 1 ГГц<br>
+								Оперативная память: 1 ГБ<br>
+								Видеокарта: c 128 МБ видеопамяти<br>
+								Жёсткий диск: 7 ГБ свободного места
+							</h3>
+						</div>
+						<div class='req'>
+							<h2>Рекомендуемые:</h2>
+							<h3>
+								Процессор: Intel Pentium 4 или AMD Athlon XP<br>
+								Оперативная память: 2 ГБ<br>
+								Видеокарта: 512 МБ видеопамяти<br>
+								Жёсткий диск: 10 ГБ свободного места<br>
+								Звуковая карта: совместимая с DirectX 9
+							</h3>
+						</div>
+					</div>
+					<div class='particle one'></div>
+					<div class='particle two'></div>
+					<div class='particle three'></div>
+					<div class='particle four'></div>
+				</div>
+				<div class='laptop'>
+					<div class='particle'></div>
+				</div>
+			</div>
+		</div>
+		<div class='footer'>
+	<div class='content'>
+		<a href='index.php'><div class='logo'></div></a>
+		<div class='menus'>
+			<ul class='menu'>
+				<a href='start.php'><li>НАЧАТЬ ИГРАТЬ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/default.htm'><li>ФОРУМ</li></a>
+				<a href='donate.php'><li>ДОНАТ</li></a>
+				<a target='_blank' href='../https@vk.com/namalskrp'><li>МЫ ВКОНТАКТЕ</li></a>
+			</ul>
+			<ul class='menu'>
+				<a href='about.php'><li>О ПРОЕКТЕ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/threads/_25D0_259F_25D1_2580_25D0_25B0_25D0_25B2_25D0_25B8_25D0_25BB_25D0_25B0-_25D0_25BF_25D1_2580_25D0_25BE_25D0_25B5_25D0_25BA_25D1_2582_25D0_25B0-namalsk-roleplay.21714/default.htm'><li>ПРАВИЛА ПРОЕКТА</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/misc/contact'><li>ОБРАТНАЯ СВЯЗЬ</li></a>
+				<a target='_blank' href='../https@forum.namalsk-rp.ru/forums/_25D0_2591_25D0_25B0_25D0_25B7_25D0_25B0-_25D0_25B7_25D0_25BD_25D0_25B0_25D0_25BD_25D0_25B8_25D0_25B9-f-a-q.6/default.htm'><li>ПОМОЩЬ</li></a>
+			</ul>
+		</div>
+	</div>
+	<h2 class='copyright'>
+		NAMALSK TEAM 2019
+	</h2>
+</div>
+<script src='js/jquery-3.2.1.min.js' type='text/javascript'></script>
+<script src='js/classie.js' type='text/javascript'></script>
+<script src='js/common.js' type='text/javascript'></script>
+<script src='js/main.js' type='text/javascript'></script>
+<!-- made by Pavel Serdyuk -->	</body>
+</html>
